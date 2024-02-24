6936cfdf31e6701fbcf09831262f4ce43c353eb6
arielgodoy <arielgodoy@gmail.com>
2024-02-24 19:21:19 -0300

    agrega process.env.PERSISTENCE

--- src/dao/factory.js
+++ src/dao/factory.js
@@ -1,25 +1,22 @@
 
 let ProductDao
 
-switch ('MONGO') {
+//dependiendo del valor de la variable de entorno PERSISTENCE, se importa el dao correspondiente
+const persistence = process.env.PERSISTENCE
+switch (persistence) {
     case 'FILE':
         //importa el dao de las entidades con persistencia en archivo
         break;
-        case 'MEMORY':
-            //importa el dao de las entidades con persistencia en memoria
-            
-            break;
-            
-            
-        default:
-            // ().default
-            // const {defa} = await import('./aslfjd')
-            // const ProductDao = (await import('ruta/del/archivo')).default
-            //importa el dao de las entidades con persistencia en base de datos
-            const ProductDaoMongo = require("./ProductDaoMongo.js");
-            ProductDao = ProductDaoMongo
+    case 'MEMORY':
+        //importa el dao de las entidades con persistencia en memoria
+        break;
+    default:
+        //importa el dao de las entidades con persistencia en base de datos
+        const ProductDaoMongo = require("./ProductDaoMongo.js");
+        ProductDao = ProductDaoMongo
         break;
 }
+
 
 module.exports = {
     ProductDao
